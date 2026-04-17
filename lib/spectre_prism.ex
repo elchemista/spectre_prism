bac32b54d1d8a107f70061e97e4133de309a23a0
Yuriy Zhar <darkyfelis@gmail.com>
2026-04-17 23:07:05 +0200

    init

---
+++ lib/spectre_prism.ex
@@ -0,0 +1,18 @@
+defmodule SpectrePrism do
+  @moduledoc """
+  Documentation for `SpectrePrism`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpectrePrism.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spectre_prism_test.exs
@@ -0,0 +1,8 @@
+defmodule SpectrePrismTest do
+  use ExUnit.Case
+  doctest SpectrePrism
+
+  test "greets the world" do
+    assert SpectrePrism.hello() == :world
+  end
+end
